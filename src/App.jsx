466171fe8ced6551b3681b90a1d5b53f66d72f51
Vicente Teixeira <vicenteteixeira05@gmail.com>
2024-10-17 17:42:33 +0100

    feat:favicon  and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
+import { Navbar } from './components/navbar';
+import { Shop } from './pages/shop/shop';
+import { Cart } from './pages/cart/cart';
+import { ShopContextProvider } from './context/shop-context';
+
 
 function App() {
+  
+
   return (
-    <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-  );
+		<div className='App'>
+		  <ShopContextProvider>
+			  
+				<Router >
+					<Navbar />
+					<Routes>
+						<Route path='/' element={<Shop />} />
+						<Route path='/cart' element={<Cart />} />
+					</Routes>
+				</Router>
+			</ShopContextProvider>
+		</div>
+	);
 }
 
 export default App
